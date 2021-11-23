55e766ee7c9b44a479d93a45b9e3019b9950a005
Vinícius Gregório <vinicius.gregorio@optera.com.br>
2021-11-22 21:28:36 -0300

    Add error_handler#handle_errors logic

--- lib/error_handler.rb
+++ lib/error_handler.rb
@@ -1,20 +1,43 @@
 module ErrorHandler
-  # TODO: implementation
-  # #handle_errors should accept a block as an argument that will execute
-  # code in the block and conditionally swallow errors given the defined rules
-  #
   # Example:
-  #
   # class MyClass
   #   include ErrorHandler
   #
   #   def perform
-  #     handle_errors do
-  #       puts "Do work..."
+  #
+  #     expected_errors = Proc.new { |e| e.class == ZeroDivisionError || e.class == NoMethodError }
+  #
+  #     # or
+  #
+  #     expected_errors = Proc.new { |e| e.message.include?('divided by 0') || e.message.include?("undefined method") }
+  #
+  #     handle_errors(expected_errors) do
+  #       result = 1 / 0
   #     end
   #   end
   # end
-  def handle_errors
-    yield
+  def handle_errors(conditions_proc, &block)
+    begin
+      block.call
+    rescue exceptions_matching(conditions_proc) => e
+      # maybe use a logger for this ...
+      puts "error_handler#handle_errors: catch an expected error! message: #{e.message}"
+      return false
+    end
   end
+
+  def exceptions_matching(conditions_proc)
+    Class.new do
+      # overwrite the triple-equals for the class
+      # inheritance chain comparison (similar to a #is_a?)
+      # and .tap to set instance variables related
+      # to the conditions_proc
+      def self.===(exception)
+        @conditions_proc.call(exception)
+      end
+    end.tap do |class_object|
+      class_object.instance_variable_set(:@conditions_proc, conditions_proc)
+    end
+  end
+
 end
--- spec/error_handler_spec.rb
+++ spec/error_handler_spec.rb
@@ -1,9 +1,34 @@
 require "error_handler"
 
 describe ErrorHandler do
+  let(:dummy_class) { Class.new { include ErrorHandler }}
+
   it "is an object" do
     expect(ErrorHandler).to be_a(Object)
   end
 
-  # TODO: please add your tests here
+  context "when catching expected errors" do
+    let(:expected_errors) { Proc.new { |e| e.class == ZeroDivisionError } }
+    let(:try_code) { Proc.new { 1 / 0 }  }
+
+    it "returns false" do
+      expect( dummy_class.new.handle_errors(expected_errors, &try_code) ).to be_falsey
+    end
+
+    it "outputs the error message" do
+      expect do
+        dummy_class.new.handle_errors(expected_errors, &try_code)
+      end.to output("error_handler#handle_errors: catch an expected error! message: divided by 0\n").to_stdout
+    end
+  end
+
+  context "when working unexpected errors" do
+    let(:expected_errors) { Proc.new { |e| e.class == ZeroDivisionError } }
+    let(:try_code) { Proc.new { 1.no_method("undefined method") }  }
+
+    it "it raises the error" do
+      expect{ dummy_class.new.handle_errors(expected_errors, &try_code) }.to raise_error(NoMethodError)
+    end
+  end
+
 end
